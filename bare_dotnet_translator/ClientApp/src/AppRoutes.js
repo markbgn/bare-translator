9b6bca47c1f6c829f16cc198d2f353a0d7ab00e0
Mark Bogan <boganmark63@gmail.com>
2023-01-23 02:16:59 +0100

    React functionality and routing. First functional version.

--- bare_dotnet_translator/ClientApp/src/AppRoutes.js
+++ bare_dotnet_translator/ClientApp/src/AppRoutes.js
@@ -1,6 +1,7 @@
 import { Counter } from "./components/Counter";
 import { FetchData } from "./components/FetchData";
 import { Home } from "./components/Home";
+import TranslateExpression from "./components/TranslateExpression";
 
 const AppRoutes = [
   {
@@ -14,6 +15,10 @@ const AppRoutes = [
   {
     path: '/fetch-data',
     element: <FetchData />
+  },
+  {
+    path: '/translate-expression',
+    element: <TranslateExpression />
   }
 ];
 
---
+++ bare_dotnet_translator/ClientApp/src/components/TranslateExpression.js
@@ -0,0 +1,32 @@
+﻿import React, { useState } from 'react';
+
+const TranslateExpression = () => {
+    const [expToTranslate, setExpToTranslate] = useState("");
+    const [expTranslated, setExpTranslated] = useState("");
+
+    // handling input change
+    const handleInputChange = (e) => {
+        setExpToTranslate(e.target.value);
+    };
+
+    // async function for fetching translation
+    async function translateHandler() {
+        if (expToTranslate !== "") {
+            const response = await fetch(`translation/${expToTranslate}`);
+            const data = await response.text();
+            setExpTranslated(data);
+        } else {
+            console.log("Input is empty");
+        }
+    }
+
+    return (
+        <main>
+            <textarea type="text" onChange={handleInputChange} value={expToTranslate} rows={3} />
+            <button onClick={translateHandler} >Translate</button>
+            <textarea type="text" readOnly="readonly" value={expTranslated} rows={3} />
+        </main>
+    )
+}
+
+export default TranslateExpression;
