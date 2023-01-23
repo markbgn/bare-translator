f6111a6bbe3954bdd41959c2501a8cef69dab675
Mark Bogan <boganmark63@gmail.com>
2023-01-23 01:37:13 +0100

    ApiController functionality

---
+++ bare_dotnet_translator/Controllers/TranslationController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bare_dotnet_translator.Controllers
+{
+    // routing for API call
+    [Route("[controller]")]
+    [ApiController]
+    public class TranslationController : ControllerBase
+    {
+        [AllowAnonymous]
+        [HttpGet("{expToTranslate}")]
+        public object Get(string expToTranslate)
+        {
+            // return value for translated expression
+            string expTranslated = "";
+
+            // instantiating a new Data Access Object for translation lookup
+            TranslationDAO translationDAO = new TranslationDAO();
+            expTranslated = translationDAO.getTranslatedExpression(expToTranslate);
+
+            return expTranslated;
+        }
+    }
+}
